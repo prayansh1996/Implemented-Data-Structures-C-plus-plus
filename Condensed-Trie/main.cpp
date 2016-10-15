6c28df06441f81c8f3f26c8b730acb1d250ece1a
prayansh1996 <prayansh1996@gmail.com>
2016-10-16 02:19:05 +0530

    Add files via upload

---
+++ Condensed-Trie/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "..\..\headers\trie2.h"
+
+int main()
+{
+    trie T,D;
+    T.insert("romane");
+    T.insert("rom");
+    T.insert("romulus");
+    T.insert("rubens");
+    T.insert("romanus");
+    T.insert("ruber");
+    T.insert("rubicon");
+    T.insert("rubicundus");
+    T.misc();
+
+    char str[10000]="The exception handler is declared with the catch keyword immediately after the closing brace of the try block.The syntax for catch is similar to a regular function with one parameter.The type of this parameter is very important,since the type of the argument passed by the throw expression is checked against it,and only in the case they match, the exception is caught by that handler$";
+    D.paragraph(str);
+    D.misc();
+    return 0;
+}
+
+
+
+
+
+
